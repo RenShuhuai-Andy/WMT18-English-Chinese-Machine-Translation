33f09bdc3729f5bd870e199a0338c6eb40b99cb6
LiLei <tobiaslee@ralee.cc>
2020-12-19 15:53:29 +0800

    update sparse transformer

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,7 @@
 from fairseq.models import register_model_architecture
 from fairseq.models.transformer import base_architecture
+from model.sparse_transformer import sparse_base_architecture
+
 
 @register_model_architecture('transformer', 'transformer_wmt_en_zh')
 def transformer_wmt_en_zh(args):
@@ -14,7 +16,6 @@ def transformer_wmt_en_zh(args):
     base_architecture(args)
 
 
-
 @register_model_architecture('transformer', 'transformer_wmt_en_zh_big')
 def transformer_wmt_en_zh_big(args):
     args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 512)
@@ -26,3 +27,31 @@ def transformer_wmt_en_zh_big(args):
     args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 8)
     args.decoder_layers = getattr(args, "decoder_layers", 6)
     base_architecture(args)
+
+
+@register_model_architecture('sparse_transformer', 'sparse_transformer_wmt_en_zh')
+def transformer_wmt_en_zh_big(args):
+    args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 512)
+    args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 1024)
+    args.encoder_attention_heads = getattr(args, "encoder_attention_heads", 4)
+    args.encoder_layers = getattr(args, "encoder_layers", 4)
+    args.decoder_embed_dim = getattr(args, "decoder_embed_dim", 512)
+    args.decoder_ffn_embed_dim = getattr(args, "decoder_ffn_embed_dim", 1024)
+    args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 4)
+    args.decoder_layers = getattr(args, "decoder_layers", 4)
+    args.top_k = getattr(args, "top_k", 8)
+    sparse_base_architecture(args)
+
+
+@register_model_architecture('sparse_transformer', 'sparse_transformer_wmt_en_zh_big')
+def transformer_wmt_en_zh_big(args):
+    args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 512)
+    args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 2048)
+    args.encoder_attention_heads = getattr(args, "encoder_attention_heads", 8)
+    args.encoder_layers = getattr(args, "encoder_layers", 6)
+    args.decoder_embed_dim = getattr(args, "decoder_embed_dim", 512)
+    args.decoder_ffn_embed_dim = getattr(args, "decoder_ffn_embed_dim", 2048)
+    args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 8)
+    args.decoder_layers = getattr(args, "decoder_layers", 6)
+    args.top_k = getattr(args, "top_k", 8)
+    sparse_base_architecture(args)
