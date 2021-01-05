3bafad4e38ad354fd351a94972741d580c59252b
Lei Li <tobiaslee@qq.com>
2021-01-05 14:25:40 +0800

    add code for get sparse attention

--- model/sparse_transformer.py
+++ model/sparse_transformer.py
@@ -168,7 +168,6 @@ class SparseTransformerModel(TransformerModel):
             decoder_embed_tokens = cls.build_embedding(
                 args, tgt_dict, args.decoder_embed_dim, args.decoder_embed_path
             )
-
         encoder = cls.build_encoder(args, src_dict, encoder_embed_tokens)
         decoder = cls.build_decoder(args, tgt_dict, decoder_embed_tokens)
         return cls(args, encoder, decoder)
@@ -227,6 +226,49 @@ class SparseTransformerDecoder(TransformerDecoder):
         if getattr(args, "checkpoint_activations", False):
             layer = checkpoint_wrapper(layer)
         return layer
+
+    def forward(
+        self,
+        prev_output_tokens,
+        encoder_out = None,
+        incremental_state = None,
+        features_only: bool = False,
+        full_context_alignment: bool = False,
+        alignment_layer= None,
+        alignment_heads = None,
+        src_lengths= None,
+        return_all_hiddens: bool = False,
+    ):
+        """
+        Args:
+            prev_output_tokens (LongTensor): previous decoder outputs of shape
+                `(batch, tgt_len)`, for teacher forcing
+            encoder_out (optional): output from the encoder, used for
+                encoder-side attention
+            incremental_state (dict): dictionary used for storing state during
+                :ref:`Incremental decoding`
+            features_only (bool, optional): only return features without
+                applying output layer (default: False).
+            full_context_alignment (bool, optional): don't apply
+                auto-regressive mask to self-attention (default: False).
+
+        Returns:
+            tuple:
+                - the decoder's output of shape `(batch, tgt_len, vocab)`
+                - a dictionary with any model-specific outputs
+        """
+        x, extra = self.extract_features(
+            prev_output_tokens,
+            encoder_out=encoder_out,
+            incremental_state=incremental_state,
+            full_context_alignment=full_context_alignment,
+            alignment_layer=alignment_layer,
+            alignment_heads=alignment_heads,
+        )
+        print(extra['attn'])
+        if not features_only:
+            x = self.output_layer(x)
+        return x, extra
 
 
 @register_model_architecture("sparse_transformer", "sparse_transformer")
